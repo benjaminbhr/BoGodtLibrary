16c8288360c02079b97435332f309ffa8cc3a8c4
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2019-12-04 10:09:38 +0100

    Imasterwindow and windowmaterialenum

---
+++ BoGodtLibrary/IMasterWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoGodtLibrary
+{
+    interface IMasterWindow
+    {
+        EWindowType GetWindowType();
+        EWindowMaterial GetWindowMaterial();
+        EColor GetColor();
+        void CanWindowOpen(bool canwindowopen);
+        bool IsWindowFrosted(bool iswindowfrosted);
+    }
+}
