3538953b98db6262c2c8012b09af9df43e8d7299
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2019-12-04 12:31:42 +0100

    d

---
+++ BoGodtLibrary/Floor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoGodtLibrary
+{
+    class Floor : IFloor
+    {
+        public EColor GetColor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EFloorType GetFloorType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
