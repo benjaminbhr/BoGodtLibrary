058eba909318ee8c5f87963fb1c89a991dae1245
Nicklas Mark Selsmark <nick6659@zbc.dk>
2019-12-16 10:24:59 +0100

    hhh

--- BoGodtLibrary/BoGodtExceptions/MaxRoomsException.cs
+++ BoGodtLibrary/BoGodtExceptions/MaxRoomsException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BoGodtLibrary.BoGodtExceptions
 {
-    class RoomException : Exception
+    public class MaxRoomsException : Exception
     {
         
     }
