de3a106b649331b8fab77183fe597a92c9f4cf35
Nicklas Mark Selsmark <nick6659@zbc.dk>
2019-12-18 12:18:48 +0100

    MinimumOneWindow + fixes

--- BoGodtLibrary/Room.cs
+++ BoGodtLibrary/Room.cs
@@ -10,21 +10,35 @@ namespace BoGodtLibrary
     public class Room : MasterRoom
     {
         private const int MAX_WINDOWS = 5;
+        private const int MIN_WINDOWS = 1;
         public override ERoomType GetRoomType()
         {
             return ERoomType.Room;
         }
 
-        public Room() { }
+        public Room() 
+        {
+            if (windows.Count + 1 < MIN_WINDOWS)
+            {
+                throw new BoGodtExceptions.MinimumOneWindowException();
+            }
+
+            IMasterWindow roomwindow = new SmallWindow();
+            roomwindow.windowFrosted = false;
+            roomwindow.windowOpen = true;
+            windows.Add(roomwindow);
+            Console.WriteLine("New window were added to the room!");
+        }
 
         public void addWindow(IMasterWindow roomwindow) 
         {
             
-            if (windows.Count +1 > MAX_WINDOWS)
+            if (windows.Count + 1 > MAX_WINDOWS)
             {
                 throw new BoGodtExceptions.OnlyFiveWindowsException();
             }
-            Console.WriteLine("windows.count: " + (windows.Count + 1));
+            
+            Console.WriteLine("windows.Count: " + (windows.Count));
             roomwindow.windowFrosted = false;
             roomwindow.windowOpen = true;
             windows.Add(roomwindow);
