07281885e8396984106f056c0cc59f9de8d0f997
Nicklas Mark Selsmark <nick6659@zbc.dk>
2019-12-18 09:46:39 +0100

    fixxx

---
+++ BoGodtLibrary/BoGodtExceptions/OnlyFiveWindowsException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoGodtLibrary.BoGodtExceptions
+{
+    public class OnlyFiveWindowsException : RoomWindowsException
+    {
+        private const string ONLYFIVEWINDOWS = "You can maximum add 5 windows to a room!";
+
+        public OnlyFiveWindowsException()
+            : base(ONLYFIVEWINDOWS)
+        {
+
+        }
+    }
+}
